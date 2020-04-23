98fa7787dd3650cf8395385623b54c169b9a3a37
Bradley <bg9892@gmail.com>
2020-04-22 22:47:34 -0500

    added switch to routes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import Section from "./components/Section";
 import Portfolio from "./components/Portfolio";
@@ -11,9 +11,12 @@ function App() {
     <Router>
       <div>
         <Nav />
-        <Route exact path="/" component={Section} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} />
+        <Switch>
+          <Route exact path="/" component={Section} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+          <Route component={Section} />
+        </Switch>
         <Footer />
       </div>
     </Router>
