9d23fd38e1f299958f5bc828fff9ece98320ec92
Bradley <bg9892@gmail.com>
2020-05-08 16:15:05 -0500

    change portfolio layout

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,6 +6,7 @@ function Portfolio() {
     <div id="main-container" class="container">
       <div class="row">
 
+
         <div class="col-md-3 mx-2 mb-2">
           <div class="card">
             <img class="card-img-top" src={process.env.PUBLIC_URL + "/Assets/Images/ScreenShot.png"} alt="Password Generator" />
@@ -18,41 +19,6 @@ function Portfolio() {
           </div>
         </div>
 
-        <div class="col-md-3 mx-2">
-          <div class="card">
-            <img class="card-img-top" src={process.env.PUBLIC_URL + "/Assets/Images/chat.png"} alt="Fireside" />
-            <div class="card-body">
-              <h5 class="card-title">Fireside</h5>
-              <p class="card-text">Fireside is a real time chat application for when you and those around you need to have a fireside chat.</p>
-              <a href="https://github.com/bg9892/Fireside" target="#" class="btn btn-primary">Github Repository</a>
-              <a href="https://powerful-crag-10383.herokuapp.com/" target="#" class="btn btn-primary ml-3" id="live">Live</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-3 mx-2 mb-2">
-          <div class="card">
-            <img class="card-img-top" src={process.env.PUBLIC_URL + "/Assets/Images/Novel-19.png"} alt="Novel-19" />
-            <div class="card-body">
-              <h5 class="card-title">Novel-19</h5>
-              <p class="card-text">Assist users in being aware of their surroundings and flattening the curve of the coronavirus.</p>
-              <a href="https://github.com/garrettmroberts/novel-19" target="#" class="btn btn-primary">Github Repository</a>
-              <a href="https://novel-19.herokuapp.com/home" target="#" class="btn btn-primary ml-3" id="live">Live</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-3 mx-2">
-          <div class="card">
-            <img class="card-img-top" src={process.env.PUBLIC_URL + "/Assets/Images/Budget-Tracker.png"} alt="Budget" />
-            <div class="card-body">
-              <h5 class="card-title">Budget Tracker</h5>
-              <p class="card-text">A budget tracking app where you can add or subtract an item, dollar amount, and track on a graph.</p>
-              <a href="https://github.com/bg9892/bootcamp-h12" target="#" class="btn btn-primary">Github Repository</a>
-              <a href="https://glacial-brook-48506.herokuapp.com/" target="#" class="btn btn-primary ml-3" id="live">Live</a>
-            </div>
-          </div>
-        </div>
 
         <div class="col-md-3 mx-2 mb-2">
           <div class="card">
@@ -66,14 +32,54 @@ function Portfolio() {
           </div>
         </div>
 
+
+        <div class="col-md-3 mx-2 mb-2">
+          <div class="card">
+            <img class="card-img-top" src={process.env.PUBLIC_URL + "/Assets/Images/Novel-19.png"} alt="Novel-19" />
+            <div class="card-body">
+              <h5 class="card-title">Novel-19</h5>
+              <p class="card-text">Assist users in being aware of their surroundings and flattening the curve of the coronavirus.</p>
+              <a href="https://github.com/garrettmroberts/novel-19" target="#" class="btn btn-primary">Github Repository</a>
+              <a href="https://novel-19.herokuapp.com/home" target="#" class="btn btn-primary ml-3" id="live">Live</a>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md-3 mx-2">
+          <div class="card">
+            <img class="card-img-top" src={process.env.PUBLIC_URL + "/Assets/Images/Budget-Tracker.png"} alt="Budget" />
+            <div class="card-body">
+              <h5 class="card-title">Budget Tracker</h5>
+              <p class="card-text">A budget tracking app where you can add or subtract an item, dollar amount, and track on a graph.</p>
+              <a href="https://github.com/bg9892/bootcamp-h12" target="#" class="btn btn-primary">Github Repository</a>
+              <a href="https://glacial-brook-48506.herokuapp.com/" target="#" class="btn btn-primary ml-3" id="live">Live</a>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md-3 mx-2">
+          <div class="card">
+            <img class="card-img-top" src={process.env.PUBLIC_URL + "/Assets/Images/chat.png"} alt="Fireside" />
+            <div class="card-body">
+              <h5 class="card-title">Fireside</h5>
+              <p class="card-text">Fireside is a real time chat application for when you and those around you need to have a fireside chat.</p>
+              <a href="https://github.com/bg9892/Fireside" target="#" class="btn btn-primary">Github Repository</a>
+              <a href="https://powerful-crag-10383.herokuapp.com/" target="#" class="btn btn-primary ml-3" id="live">Live</a>
+            </div>
+          </div>
+        </div>
+
+
         <div class="col-md-3 mx-2">
           <div class="card">
             <img class="card-img-top" id="novel" src="https://via.placeholder.com/200x200" alt="Novel-19" />
             <div class="card-body">
               <h5 class="card-title">Comming soon</h5>
               <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesquefelis arcu, maximus vel urna sed.</p>
-              <a href="https://bg9892.github.io/portfolio" target="#" class="btn btn-primary">Github Repository</a>
-              <a href="https://bg9892.github.io/portfolio" target="#" class="btn btn-primary ml-3" id="live">Live</a>
+              <a href="https://bg9892.github.io/" target="#" class="btn btn-primary">Github Repository</a>
+              <a href="https://bg9892.github.io/" target="#" class="btn btn-primary ml-3" id="live">Live</a>
             </div>
           </div>
         </div>
