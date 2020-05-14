183fbec6ebdac4514d29e39442d4785fd93ff54d
Bradley <bg9892@gmail.com>
2020-05-14 17:51:04 -0500

    updated about summary

---
+++ build/precache-manifest.743df96b71eeae2b9fd70b37997cf3d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4b6b0efad55d752842d86b55d92bab",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "6e9cd06f2fb09a8f8e08",
+    "url": "/react-portfolio/static/css/2.9de105d4.chunk.css"
+  },
+  {
+    "revision": "0307ca094d7485050ad8",
+    "url": "/react-portfolio/static/css/main.ac73ed28.chunk.css"
+  },
+  {
+    "revision": "6e9cd06f2fb09a8f8e08",
+    "url": "/react-portfolio/static/js/2.97c38d39.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/react-portfolio/static/js/2.97c38d39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0307ca094d7485050ad8",
+    "url": "/react-portfolio/static/js/main.d6526a64.chunk.js"
+  },
+  {
+    "revision": "a229530a0c0d1ed130b6",
+    "url": "/react-portfolio/static/js/runtime-main.65d8297c.js"
+  }
+]);
--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -12,10 +12,9 @@ function Section() {
             alt="profile" width="200" height="200" />
           <p class="text-left">
             I’m from Nashville Tennessee, I went to Tennessee College of Applied Technology in 2013 majoring in
-            mechatronics and industrial automation. I started playing around with .net and made a few small apps
-            in C# and Visual Basic. I decided I wanted a career in the web development field. I am currently enrolled
-            in a full stack web development program at Vanderbilt Coding Bootcamp where we are learning HTML, CSS,
-            Javascript, jQuery, API's, Node.js, MongoDB, Express, and React.
+            mechatronics and industrial automation. I started learning .net and made a few small apps
+            in C# and Visual Basic. I decided I wanted a career in the web development field. I am a graduate from Vanderbilt University's full stack web development bootcamp and proficient in HTML5, CSS3,
+            Javascript, jQuery, Bootstrap, API's, Node.js, MongoDB, mySQL, AJAX, Express, React.js, GIT, GitHub, and Heroku.
           </p>
         </div>
       </div>
